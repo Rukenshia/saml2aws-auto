c104d1c86d1f34fc9f165c7ec076f5fd5dc89f03
Jan Christopehrsen <jan@ruken.pw>
2018-04-15 18:45:29 +0200

    Improved refresh UX

--- src/refresh.rs
+++ src/refresh.rs
@@ -7,21 +7,29 @@ use config;
 use saml2aws::Saml2Aws;
 
 pub fn command(matches: &ArgMatches) {
-    let group = matches.value_of("GROUP").unwrap();
+    let group_name = matches.value_of("GROUP").unwrap();
     let mfa = matches.value_of("mfa").unwrap();
     let cfg = config::load_or_default().unwrap();
 
-    let group = match cfg.groups.get(group) {
+    let group = match cfg.groups.get(group_name) {
         Some(g) => g,
         None => {
             println!(
                 "\nCould not refresh credentials for {}:\n\n\t{}\n",
-                paint(group).with(Color::Yellow),
+                paint(group_name).with(Color::Yellow),
                 paint("The specified group does not exist.").with(Color::Red)
             );
             return;
         }
     };
+
+    if group.accounts.len() == 0 {
+        println!(
+            "Nothing to refresh. Group {} is empty.",
+            paint(group_name).with(Color::Yellow)
+        );
+        return;
+    }
 
     let s = Saml2Aws::new();
     let mut errors = vec![];
@@ -47,5 +55,13 @@ pub fn command(matches: &ArgMatches) {
         for error in &errors {
             println!("\t{}", paint(error.description()).with(Color::Red));
         }
+
+        return;
     }
+
+    println!("\nRefreshed group {}. To use them in the AWS cli, apply the --profile flag with the name of the account.", paint(group_name).with(Color::Yellow));
+    println!(
+        "\nExample:\n\n\taws --profile {} s3 ls\n",
+        paint(&group.accounts[0].name).with(Color::Yellow).bold()
+    );
 }
