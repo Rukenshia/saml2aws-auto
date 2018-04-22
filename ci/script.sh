817171d388a91d63d9f8f2daaa3b49106dc00a3b
Jan Christopehrsen <jan@ruken.pw>
2018-04-22 16:21:01 +0200

    Update cargo run command

--- ci/script.sh
+++ ci/script.sh
@@ -14,8 +14,8 @@ main() {
     cross test --target $TARGET --release
 
     shell_session_update() { :; }
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    cross run --target $TARGET -- groups list
+    cross run --target $TARGET --release -- groups list
 }
 
 # we don't run the "test phase" when doing deploys
