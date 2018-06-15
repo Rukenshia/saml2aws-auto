093626920f27ddac51dd8a2553afac8d8657110b
Jan Christophersen <jan@ruken.pw>
2018-06-15 12:36:56 +0200

    Improve error handling

--- Casks/checkpoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-brew cask _appcast_checkpoint --calculate "https://github.com/rukenshia/saml2aws-auto/releases.atom"
--- src/aws/assume_role.rs
+++ src/aws/assume_role.rs
@@ -1,14 +1,16 @@
 use aws::xml::{AssumeRoleResponse, AssumeRoleResult, Credentials};
-use reqwest::{Client, Error};
+use reqwest::{Client, Response, StatusCode};
 use serde_xml_rs;
+use std::error::Error;
+use std::io;
 
 pub fn assume_role(
     arn: &str,
     principal: &str,
     saml_assertion: &str,
     session_duration: Option<i64>,
-) -> Result<Credentials, Error> {
-    let mut res = match Client::new()
+) -> Result<Credentials, impl Error> {
+    let mut res: Response = match Client::new()
         .post("https://sts.amazonaws.com/")
         .query(&[
             ("Version", "2011-06-15"),
@@ -16,21 +18,28 @@ pub fn assume_role(
             ("PrincipalArn", principal),
             ("RoleArn", arn),
             ("SAMLAssertion", saml_assertion),
+            (
+                "DurationSeconds",
+                &format!("{}", session_duration.or(Some(3600)).unwrap()),
+            ),
         ])
         .send()
     {
         Ok(r) => r,
         Err(e) => {
-            return Err(e);
+            return Err(io::Error::new(io::ErrorKind::Other, e.description()));
         }
     };
 
-    let text = res
-        .text()
-        .unwrap()
-        .replace(" xmlns=\"https://sts.amazonaws.com/doc/2011-06-15/\"", "");
+    if res.status() != StatusCode::Ok {
+        println!("response: '{:?}'", res.text());
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("sts assume role returned {}", res.status()),
+        ));
+    }
 
-    println!("text is {}", text);
+    let text = res.text().unwrap();
 
     let response: AssumeRoleResponse = serde_xml_rs::deserialize(text.as_bytes()).unwrap();
 
@@ -44,7 +53,7 @@ pub fn assume_role(
 
     let credentials = match credentials {
         AssumeRoleResult::Credentials(ref c) => c,
-        _ => panic!("wtf"),
+        _ => panic!("this should never, ever happen. It did. awesome."),
     };
 
     Ok(credentials.clone())
