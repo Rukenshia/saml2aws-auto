a5ec2f92561d19fabc7d5413ba6df3ad079af20e
Jan Christophersen <jan@ruken.pw>
2018-06-27 17:20:20 +0200

    Automatically create .aws directory

--- src/aws/credentials.rs
+++ src/aws/credentials.rs
@@ -1,11 +1,19 @@
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
 use std::path::PathBuf;
 
 use super::ini;
 
 pub fn load_credentials_file() -> Result<(ini::Ini, PathBuf), ini::ini::Error> {
-    let filename = env::home_dir().unwrap().join(".aws").join("credentials");
+    let filename = env::home_dir().unwrap().join(".aws");
+
+    if !filename.exists() {
+        fs::create_dir(filename).expect(
+            "Could not create $HOME/.aws directory. Please check permissions"
+        );
+    }
+    
+    let filename = filename.join("credentials");
 
     if !filename.exists() {
         File::create(&filename).expect(
